feb0c3f174cec62b3aad0daeaae457af718f062d
Sem <sem.roig@c-tech.com.ar>
2021-04-30 19:10:03 -0300

    Agrego templates

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "webapp"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register", views.register, name="register"),
-    path("home", views.home, name="home"),
-    path("login", views.login, name="login")
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,41 +2,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
 from django.urls import reverse
-
-class NewUserForm(forms.Form):
-    username = forms.CharField(label="Username")
-    password = forms.CharField(label="Password")
-    rep_password = forms.CharField(label="Repet password")
+from django.contrib.auth import authenticate, login, logout
 
 class LogInForm(forms.Form):
     username = forms.CharField(label="Username")
     password = forms.CharField(label="Password")
 
 def index(request):
-    return render(request, "webapp/index.html")
-
-def register(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            new_username = form.cleaned_data["username"]
-            new_password = form.cleaned_data["password"]
-            return HttpResponseRedirect(reverse("home"))
-        else:
-            return render(request, "webapp/register.html", {
-                "form": form
-            })
-    return render(request, "webapp/register.html", {
-        "form": NewUserForm()
+    if not request.user.is_authenticated:
+        return render(request, "webapp/index.html")
+    return render(request, "webapp/index.html", {
+        "message": "You are logged in! Start paying please."
     })
 
-def login(request):
+def login_view(request):
     if request.method == "POST":
         form = LogInForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
-            return HttpResponseRedirect(reverse("home"))
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("webapp:index"))
+            else:
+                return render(request, "webapp/login.html", {
+                    "message": "Invalid credentials."
+                })
         else:
             return render(request, "webapp/login.html", {
                 "form": form
@@ -44,6 +36,7 @@ def login(request):
     return render(request, "webapp/login.html", {
         "form": LogInForm()
     })
-    
-def home(request):
-    return render(request, "webapp/home.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("webapp:index"))
